b2ce94329b3e44e0f5c6cf35f08224064de1a99c
Steven Epstein <sbepstein42@gmail.com>
2021-12-17 16:25:50 -0500

    combined two functions

--- sales_report.py
+++ sales_report.py
@@ -1,17 +1,22 @@
 """Generate sales report showing total melons each salesperson sold."""
 # Working on the exercise at https://ed.devmountain.com/materials/data-bp-1/exercises/salesperson-report/
 
-def open_file(file_name):
-    """This function opens the file and saves it to a variable."""
-    return open(file_name)
-
-def compiler (file_txt):
-    """This function reads through a file and compiles it into 2 lists, one for salespople and one for melons_sold, with a direct relation between matching indexes, such as salespeople[2] and melons_sold[2]. Then returns both of those lists."""
+def compiler (file_name):
+    """This function opens and reads through a file and compiles it into 2 lists, one for salespople and one for melons_sold, with a direct relation between matching indexes, such as salespeople[2] and melons_sold[2].
+    
+    Argument:
+        file_name (str) - name of the .txt file to work with
+    
+    Returns:
+        salespeople (list)
+        melons_sold (list)
+    """
     
     salespeople = []
     melons_sold = []
 
-    for line in file_txt:
+    f = open(file_name)
+    for line in f:
         salesperson, _, melons = line.rstrip().split('|')
 
         if salesperson in salespeople:
@@ -24,13 +29,16 @@ def compiler (file_txt):
     return salespeople, melons_sold
 
 def printer(salespeople, items_sold):
-    """This function takes 2 lists as a parameter, salespeople and items sold, and then prints that information in a more readalbe format in the terminal."""
+    """This function takes 2 lists as a parameter, salespeople and items sold, and then prints that information in a more readalbe format in the terminal.
+    
+    Arguments:
+        salespeople (list)
+        items_sold (list)
+    """
     for i in range(len(salespeople)):
         print(f'{salespeople[i]} sold {items_sold[i]} melons')
 
 
-file_txt = open_file('sales-report.txt')
-
-salespeople, melons_sold = compiler(file_txt)
+salespeople, melons_sold = compiler('sales-report.txt')
 
 printer(salespeople, melons_sold)
