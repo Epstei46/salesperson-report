dfb3666958572e869dd8d08a342c4e7c5a696399
Steven Epstein <sbepstein42@gmail.com>
2021-12-17 16:14:01 -0500

    refactored code into functions

--- sales_report.py
+++ sales_report.py
@@ -1,25 +1,36 @@
 """Generate sales report showing total melons each salesperson sold."""
+# Working on the exercise at https://ed.devmountain.com/materials/data-bp-1/exercises/salesperson-report/
+
+def open_file(file_name):
+    """This function opens the file and saves it to a variable."""
+    return open(file_name)
+
+def compiler (file_txt):
+    """This function reads through a file and compiles it into 2 lists, one for salespople and one for melons_sold, with a direct relation between matching indexes, such as salespeople[2] and melons_sold[2]. Then returns both of those lists."""
+    
+    salespeople = []
+    melons_sold = []
+
+    for line in file_txt:
+        salesperson, _, melons = line.rstrip().split('|')
+
+        if salesperson in salespeople:
+            position = salespeople.index(salesperson)
+
+            melons_sold[position] += int(melons)
+        else:
+            salespeople.append(salesperson)
+            melons_sold.append(int(melons))
+    return salespeople, melons_sold
+
+def printer(salespeople, items_sold):
+    """This function takes 2 lists as a parameter, salespeople and items sold, and then prints that information in a more readalbe format in the terminal."""
+    for i in range(len(salespeople)):
+        print(f'{salespeople[i]} sold {items_sold[i]} melons')
 
 
-salespeople = []
-melons_sold = []
+file_txt = open_file('sales-report.txt')
 
-f = open('sales-report.txt')
-for line in f:
-    line = line.rstrip()
-    entries = line.split('|')
+salespeople, melons_sold = compiler(file_txt)
 
-    salesperson = entries[0]
-    melons = int(entries[2])
-
-    if salesperson in salespeople:
-        position = salespeople.index(salesperson)
-
-        melons_sold[position] += melons
-    else:
-        salespeople.append(salesperson)
-        melons_sold.append(melons)
-
-
-for i in range(len(salespeople)):
-    print(f'{salespeople[i]} sold {melons_sold[i]} melons')
+printer(salespeople, melons_sold)
